779a7e31e34cbc484d9e38a8e14304b3b9704da7
JoeCalvi <joecalvi@me.com>
2023-07-04 11:55:25 -0600

    class

---
+++ includes/autoloader.inc.php
@@ -0,0 +1,25 @@
+<?php
+spl_autoload_register('myAutoLoader');
+
+// loads classes automatically as soon as class is instantiated
+function myAutoLoader($className) {
+  $url = $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+
+// ensures correct pathing 
+  if(strpos($url, 'includes') !== false) {
+    $root = '../classes';
+  } else {
+    $root = "classes/";
+  }
+
+  $ext = ".class.php";
+  $path = $root . $className . $ext;
+
+// returns a cleaner, more concise error message
+  if(!file_exists($path)) {
+    return false;
+  }
+
+  require_once $path;
+}
+?>
--- index.php
+++ index.php
@@ -1,14 +1,6 @@
 <?php
-spl_autoload_register('myAutoLoader');
-
-// loads classes automatically as soon as class is instantiated
-function myAutoLoader($className) {
-  $root = "classes/";
-  $ext = ".class.php";
-  $path = $root . $className . $ext;
-
-  include_once $path;
-}
+declare(strict_types = 1);
+include 'includes/autoloader.inc.php';
 ?>
 
 <!DOCTYPE html>
@@ -16,9 +8,22 @@ function myAutoLoader($className) {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Calculator</title>
 </head>
 <body>
+
+  <form action="includes/calc.inc.php" method="post">
+    <p>My own calculator!</p>
+    <input type="number" name="num1" placeholder="First Number">
+    <select name="oper">
+        <option value="add">Addition</option>
+        <option value="sub">Subtraction</option>
+        <option value="div">Division</option>
+        <option value="mul">Multiplication</option>
+    </select>
+    <input type="number" name="num2" placeholder="Second Number">
+    <button type="submit" name="submit">Calculate!</button>
+  </form>
   
 </body>
 </html>
